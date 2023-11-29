0cbcd3d7fd383472501b29c134c3287839533a58
geb0598 <geb0598@gmail.com>
2023-11-30 02:18:28 +0900

    refactor: Refactoring Bullet WIP

--- Assets/Scripts/Player/GrazePoint.cs
+++ Assets/Scripts/Player/GrazePoint.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class GrazePoint : MonoBehaviour
 {
+    [SerializeField] float _minExpGenerationRange;
+    [SerializeField] float _maxExpGenerationRange;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            // generate EXP item
-            Debug.Log("EXP");
+            GenerateExpItem();
         }
+    }
+
+    private void GenerateExpItem()
+    {
+        float length = Random.Range(_minExpGenerationRange, _maxExpGenerationRange);
+        Vector2 direction = Random.insideUnitCircle.normalized;
+        Vector2 position = (Vector2)transform.position + length * direction;
+
+        Exp exp = GameManager.GetInstance().poolManager.GetComponent<Exp>();
+        exp.transform.position = position;
+        exp.Init(1);
     }
 }
--- Assets/Scripts/Player/ItemGatheringPoint.cs
+++ Assets/Scripts/Player/ItemGatheringPoint.cs
@@ -4,18 +4,13 @@ using UnityEngine;
 
 public class ItemGatheringPoint : MonoBehaviour
 {
-    [SerializeField] private float _gatheringRange;
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    [SerializeField] private float _itemGatheringSpeed;
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Item"))
+        if (collision.CompareTag("Item"))
         {
-
+            Vector2 direction = (collision.transform.position - transform.position).normalized;
+            collision.GetComponent<Rigidbody2D>().MovePosition(direction * _itemGatheringSpeed);
         }
-    }
-
-    private void GatherItem(GameObject item)
-    {
-        
     }
 }
